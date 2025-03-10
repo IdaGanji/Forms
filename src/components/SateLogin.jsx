775bb7c4fe9f3d6d551984668a5820239bd32bcc
Ida Ganji <ida.ganji@omegapoint.no>
2025-03-10 15:28:03 +0100

    Added onBlur to see when the user is finished typing and only then show the error mesage.

--- src/components/SateLogin.jsx
+++ src/components/SateLogin.jsx
@@ -4,7 +4,14 @@ export default function Login() {
     email: '',
     password: ''
   });
-  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@')
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  });
+  // the onBlur event helps us know which input field and when it has been touched and finished with
+  // So the user gets a good chance to complete the entering before getting an error message
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@')
+
 
   // JS Dynamic key vs Fixed key:  
   // const keyName = "age";
@@ -18,6 +25,12 @@ export default function Login() {
       [identifier]: event.target.value
     })
     )
+    // we set the didEdit for the field that is being typed in to false so that emailIsInvalid doesn´t get true when the user is typing
+    // only after the user has finished
+    setDidEdit((prevState) => ({
+      ...prevState,
+      [identifier]: false
+    }))
   }
   // Validating on every key-stroke
   // problem is that the error will be shown too early for this usecase
@@ -29,6 +42,14 @@ export default function Login() {
     //   password: ''
     // })
   }
+  // event that gets fired when an element loses focus
+  const handleBlur = (identifier) => {
+    setDidEdit((prevState) => ({
+      ...prevState,
+      [identifier]: true
+    }))
+
+  }
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -36,7 +57,8 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleInputChange("email", event)} value={enteredValues.email} />
+          <input id="email" type="email" name="email" onChange={(event) => handleInputChange("email", event)} value={enteredValues.email}
+            onBlur={() => handleBlur('email')} />
           <div className="control-error">{emailIsInvalid && <p>Please add valid email address</p>}</div>
         </div>
 
